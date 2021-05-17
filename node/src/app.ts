3cb585107a574c9145793139be4aead972d33f90
= <carloshehe@Carloss-Air.lan>
2021-05-17 16:44:51 -0700

    Finished dockerizing application

--- node/src/app.ts
+++ node/src/app.ts
@@ -3,7 +3,7 @@ import express, {Request, Response} from 'express';
 const app: express.Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello from Carlos: ya tu sabee");
+    res.send("Hello from Carlos: siiii mi vidaaa");
 }) 
 
 app.listen(5000,() => {console.log("Running Carlos's server on Port 5000")})
