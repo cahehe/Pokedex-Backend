2a2770f600283a66b18c8bf2edb603194e83751e
= <carloshehe@Carloss-Air.lan>
2021-05-21 14:47:12 -0700

    Moved all db code to db.ts

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,24 +1,5 @@
-import express, {Request, Response} from 'express';
-import pool from './db';
 
-const app: express.Application = express();
-
-app.use(express.json()) //used to request body
-
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello from Carlos:");
-}) 
-
-//get all 
-app.get('/all', async (req: Request, res: Response) => {
-    try{
-        const all = await pool.query("SELECT * FROM GENERAL WHERE POKEDEX_NUMBER < 10; ")
-        res.json(all.rows);
-    }
-    catch(err: any){
-        console.log(err.message);
-    }
-})
+import app from './db'
 
 app.listen(5000,() => {console.log("Running Carlos's server on Port 5000")})
 
--- node/src/db.ts
+++ node/src/db.ts
@@ -1,4 +1,5 @@
 import pg, { Pool } from 'pg';
+import express, {Request, Response} from 'express';
 
 const pool: Pool = new pg.Pool({
     user: 'carloshehe',
@@ -8,5 +9,24 @@ const pool: Pool = new pg.Pool({
     port: 5432
 });
 
+const app: express.Application = express();
+
+app.use(express.json()) //used to request body
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello from Carlos:");
+}) 
+
+//get all 
+app.get('/all', async (req: Request, res: Response) => {
+    try{
+        const all = await pool.query("SELECT * FROM GENERAL WHERE POKEDEX_NUMBER < 10; ")
+        res.json(all.rows);
+    }
+    catch(err: any){
+        console.log(err.message);
+    }
+})
+
 //export default module.exports = pool;
-export default pool;
+export default app;
