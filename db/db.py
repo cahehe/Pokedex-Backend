d067b443b3dd59374c5c8d31de95490e773f8e80
= <carloshehe@Carloss-MacBook-Air.local>
2021-08-06 16:26:10 -0700

    hid away db credentials

--- db/db.py
+++ db/db.py
@@ -1,7 +1,11 @@
-import psycopg2
+import psycopg2, json
+
+file = open('../node/config.json')
+
+credentials = json.load(file)
 
 def postgres():    
-    confg = 'dbname=drdf2a9fuogo2 host=ec2-52-0-67-144.compute-1.amazonaws.com port=5432 user=irfppesxyddqtp password=04b863cb25ea01d6152228daffd4b78876b8c4ddc5ccfdd300a13b237ea3e2ec sslmode=require'
+    confg = credentials["heroku"]["CONNECTION_STR"]
     connect = psycopg2.connect(confg)    
     return connect
     # cur.execute("""Select * from alldata;""")
